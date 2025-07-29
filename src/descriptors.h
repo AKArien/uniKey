87c71ae3a86cdab278b52ea14528fdc89040f701
Hue <emilien.dessarps@gmail.com>
2025-07-29 13:47:04 +0200

    rough draft of sequence

--- src/descriptors.h
+++ src/descriptors.h
@@ -4,7 +4,7 @@
 // device and configuration descritors are handled in usb config…
 // todo : verify i’m not talking out of my ass
 
-const PROGMEM char usbDescriptorConfiguration [] = { // i think, i think, that this is the name vusb wants it in
+const PROGMEM char normal_descriptor_conf[] = { // i think, i think, that this is the name vusb wants it in
 	0x00, 0x09, // bLength
 	0x00, 0x21, // bDescriptorType
 	0x00, 0x00, 0x01, 0x11, // bcdHID
@@ -14,40 +14,73 @@ const PROGMEM char usbDescriptorConfiguration [] = { // i think, i think, that t
 	0x00, 0x00, 0x00, 0x4F // wDescriptorLength
 };
 
-#define MAX_KEYS_REPORTED_AT_ONCE 3
-#define MAX_MODS_REPORTDE_AT_ONCE 5
+const PROGMEM char erglacon_descriptor_conf[] = { // i think, i think, that this is the name vusb wants it in
+	0x00, 0x09, // bLength
+	0x00, 0x21, // bDescriptorType
+	0x00, 0x00, 0x01, 0x11, // bcdHID
+	0x00, 0x08, // bCountryCode france
+	0x00, 0x01, // bNumDescriptors
+	0x00, 0x22, // wDescriptorType
+	0x00, 0x00, 0x00, 0x4F // wDescriptorLength
+};
 
-const PROGMEM char usbHidReportDescriptor[79] = { // vusb wants it in this name
+char *usbDescriptorConfiguration;
+
+const PROGMEM char usbHidReportDescriptor_normal[39] = {
 	0x05, 0x01, // Usage Page (Generic Desktop Page)
 	0x09, 0x06, // Usage (Keyboard)
 	0xA1, 0x01, // Collection (Application)
-	0x05, 0x07, 	// Usage Page (key codes)
 
 	// main keys
+	0x05, 0x07, 	// Usage Page (key codes)
 	0x19, 0x04, 	// Usage Minimum (a)
 	0x29, 0x39, 	// Usage Maximum (caps lock)
 	0x15, 0x00, 	// Logical Min (0)
 	0x25, 0x01, 	// Logical Max (1)
 	0x75, 0x01, 	// Report Size (1)
-	0x95, MAX_KEYS_REPORTED_AT_ONCE, 	// Report Count ()
+	0x95, 0x03, 	// Report Count ()
 	0x81, 0x02, 	// Input(Data, Var, Abs)
 
 	// modifiers
+	0x05, 0x07, 	// Usage Page (key codes)
 	0x19, 0xE0, 	// Usage Minimum (lctl)
 	0x29, 0x07, 	// Usage Maximum (rmeta)
 	0x15, 0x00, 	// Logical Min (0)
 	0x25, 0x01, 	// Logical Max (1)
 	0x75, 0x01, 	// Report Size (1)
-	0x95, MAX_MODS_REPORTDE_AT_ONCE, 	// Report Count ()
+	0x95, 0x05, 	// Report Count ()
 	0x81, 0x02, 	// Input(Data, Array, Abs)
 
 	0xC0        // End Collection
 };
 
-// struct report_data{
-// 	uint8_t keys[MAX_KEYS_REPORTED_AT_ONCE];
-// 	uint8_t mods[MAX_MODS_REPORTDE_AT_ONCE];
-// };
-// Would be nice to do that, but…
+const PROGMEM char usbHidReportDescriptor_erglacon[41] = {
+	0x05, 0x01, // Usage Page (Generic Desktop Page)
+	0x09, 0x06, // Usage (Keyboard)
+	0xA1, 0x01, // Collection (Application)
+
+	// main keys
+	0x05, 0x10, 	// Usage Page (unicode)
+	0x19, 0x00, 0x61, 	// minimum (a)
+	0x29, 0x20, 0x2F, 	// maximum (narrow no break space)
+	0x15, 0x00, 	// Logical Min (0)
+	0x25, 0x01, 	// Logical Max (1)
+	0x75, 0x01, 	// Report Size (1)
+	0x95, 0x02, 	// Report Count (2)
+
+	// modifiers
+	0x05, 0x07, 	// Usage Page (key codes)
+	0x19, 0xE0, 	// Usage Minimum (lctl)
+	0x29, 0x07, 	// Usage Maximum (rmeta)
+	0x15, 0x00, 	// Logical Min (0)
+	0x25, 0x01, 	// Logical Max (1)
+	0x75, 0x01, 	// Report Size (1)
+	0x95, 0x04, 	// Report Count (4)
+	0x81, 0x02, 	// Input(Data, Array, Abs)
+
+	0xC0        // End Collection
+};
+
+char *usbHidReportDescriptor;
 
 #endif // descriptors_h_INCLUDED
--- src/main.c
+++ src/main.c
@@ -1,48 +1,103 @@
-// #define F_CPU 16000000
-
 #include <avr/io.h>
 #include <avr/interrupt.h>
 #include <avr/pgmspace.h>
 #include <avr/wdt.h>
 #include <util/delay.h>
 #include <string.h>
+#include <stdlib.h>
 
 #include "usbdrv/usbdrv.h"
 
 #include "descriptors.h"
-#include "keycodes/codes.h"
-#include "keycodes/azerty.h"
-#include "keycodes/erglacon.h"
+#include "keys/layouts.h"
+
+uint8_t*** current_layout = (uint8_t***)normal_table;
 
 uint8_t* report_buffer;
 
-uint8_t idle_rate = 500;
+uint8_t* keys_buffer_hist[3];
+uint8_t keys_buffer_size;
+uint8_t keys_buffer_pos;
+
+uint16_t* unic_buffer_hist[2];
+uint8_t unic_buffer_size;
+uint8_t unic_buffer_pos;
+
+uint8_t* mods_buffer_hist;
+uint8_t mods_buffer_size;
+uint8_t mods_buffer_pos;
+
+
+
+int idle_rate = 500;
+
+enum modes {
+	normal,
+	erglacon
+};
 
 
 void init(){
 	// vusb is on 2 and 4 of D, since it needs an interrupt (D2, aka int0)
 
-	// the other interrupt pin is D3 (int1), which is used to execute updateReportBuffer upon any key being pressed
-
-	// B0-7 are connected to the key matrix (8 > sqrt(61), which is how many keys i shuold use if i counted correctly
-	
+	// B0-7 and C0-7 are connected to the key matrix (8*8 > 61, which is how many keys i shuold use if i counted correctly. The p  
+	DDRC = 0xFF; // write
+	PORTC = 0x00;
+	DDRB = 0x00; // read
 }
 
-// ISR(PCINT1_vect){
-// 	snapshot_inputs();
-// }
+void update_keys_buffer_normal(int code){
+	*keys_buffer_hist[keys_buffer_pos%keys_buffer_size] = (uint8_t)code;
+	keys_buffer_pos = (keys_buffer_pos + 1) % keys_buffer_size;
+}
+
+void update_keys_buffer_unicode(int code){
+	*unic_buffer_hist[unic_buffer_pos%unic_buffer_size] = (uint16_t)code;
+	unic_buffer_pos = (unic_buffer_pos + 1) % unic_buffer_size;
+}
+
+void (*update_keys_buffer_current)(int);
 
 void updateReportBuffer(){
 	// Read matrix
-	PINB
-	
-	
-	// Clear unpressed keys from last time
-	
+	for (int i = 0 ; i < 8 ; i++){
+		PORTC = 0x01 << i;
+		for (int a = 0 ; a < 8 ; a++){
+			if ((PINB >> a) & 1){
+				int code = *current_layout[i][a];
+				switch (code){
+					case LCTL: case LSFT: case LALT: case LMTA: case RCTL: case RSFT: case RALT: case RMTA: 
+						mods_buffer_hist[mods_buffer_pos%mods_buffer_size] = code;
+						mods_buffer_pos = (mods_buffer_pos + 1) % mods_buffer_size;
 
-	// Override oldest entry
+					default:
+						update_keys_buffer_current(code);
+				}
+			}
+		}
+	}
+	PORTC = 0x00; // get reset loser
+}
 
-	
+void set_mode(enum modes mode){
+	if (keys_buffer_hist){
+		free(keys_buffer_hist);
+		free(mods_buffer_hist);
+	}
+	keys_buffer_pos = 0;
+	unic_buffer_pos = 0;
+	mods_buffer_pos = 0;
+
+	if (mode == normal){
+		mods_buffer_hist = malloc(sizeof(uint8_t)*5);
+		mods_buffer_size = 5;
+		update_keys_buffer_current = &update_keys_buffer_normal;
+	}
+	else {
+		mods_buffer_hist = malloc(sizeof(uint8_t)*4);
+		mods_buffer_size = 4;
+		update_keys_buffer_current = &update_keys_buffer_unicode;
+	}
 }
 
 usbMsgLen_t usbFunctionSetup(uint8_t data[8]){
